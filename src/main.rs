6245c61226e528f971ab5b006eb63a54cd90f29b
darknight-hk <ha986523@gmail.com>
2026-02-09 18:46:32 +0500

    Fix checksum verification output and improve error handling in main function

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,13 @@ use crate::cartridge::Cartridge;
 
 fn main() {
     let cart = Cartridge::new("sml.gb");
-    println!("{:#?}", cart.unwrap().header);
+    println!("{:#?}", cart.as_ref().unwrap().header);
+    match cart.unwrap().verify_checksum() {
+        true => {  
+            println!("Verifcation Successful!");
+        }
+        false => {
+            println!("Failed to verify checksum");
+        }
+    }
 }
