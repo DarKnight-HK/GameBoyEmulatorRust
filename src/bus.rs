7ec6ea8ccd09d44d4edba2a8d98a197d2f974dbf
darknight-hk <ha986523@gmail.com>
2026-02-10 21:07:23 +0500

    Refactor Bus structure to use Cartridge instead of Mmu and implement read/write methods for memory management

--- src/bus.rs
+++ src/bus.rs
@@ -1,22 +1,55 @@
-use crate::mmu::Mmu;
+use crate::cartridge::Cartridge;
 
 pub struct Bus {
-    mmu: Mmu,
+    cartridge: Cartridge,
+
+    vram: [u8; 8192],
+    wram: [u8; 8192],
+    hram: [u8; 127],
 }
 
 impl Bus {
-    pub fn new() -> Self {
+    pub fn new(cartridge: Cartridge) -> Self {
         Bus {
-            mmu: Mmu::new(),
+            cartridge,
+            vram: [0; 8192],
+            wram: [0; 8192],
+            hram: [0; 127],
         }
     }
 
-    // The CPU will call this to read/write
-    pub fn read_byte(&self, addr: u16) -> u8 {
-        self.mmu.read(addr)
+    pub fn read_byte(&self, address: u16) -> u8 {
+        match address {
+            0x0000..=0x7FFF => self.cartridge.read(address),
+
+            0x8000..=0x9FFF => self.vram[(address - 0x8000) as usize],
+
+            0xA000..=0xBFFF => self.cartridge.read(address),
+
+            0xC000..=0xDFFF => self.wram[(address - 0xC000) as usize],
+
+            0xE000..=0xFDFF => self.wram[(address - 0xE000) as usize],
+
+            0xFE00..=0xFE9F => 0x00,
+
+            0xFEA0..=0xFEFF => 0x00,
+
+            0xFF00..=0xFF7F => 0x00,
+
+            0xFF80..=0xFFFE => self.hram[(address - 0xFF80) as usize],
+
+            0xFFFF => 0x00,
+        }
     }
 
-    pub fn write_byte(&mut self, addr: u16, value: u8) {
-        self.mmu.write(addr, value);
+    pub fn write_byte(&mut self, address: u16, byte: u8) {
+        match address {
+            0x0000..=0x7FFF => { /* Handle MBC Later */ }
+            0x8000..=0x9FFF => self.vram[(address - 0x8000) as usize] = byte,
+            0xA000..=0xBFFF => { /* Handled by cartridge */ }
+            0xC000..=0xDFFF => self.wram[(address - 0xC000) as usize] = byte,
+
+            _ => {}
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 mod bus;
 mod cartridge;
 mod cpu;
-mod mmu;
 mod ppu;
 
 use bus::Bus;
 use cpu::Cpu;
-use mmu::Mmu;
+
 
 use crate::cartridge::Cartridge;
 
 fn main() {
-    let cart = Cartridge::new("sml.gb");
+    let cart = Cartridge::new("tetris.gb");
     println!("{:#?}", cart.as_ref().unwrap().header);
     match cart.unwrap().verify_checksum() {
         true => {  
