86320fa71be7376a96bd1742fefb67ff84560609
darknight-hk <ha986523@gmail.com>
2026-02-15 13:15:55 +0500

    Refactor GameBoyEmulator project structure and update dependencies
    - Updated Cargo.toml to specify edition as 2021 and added dependencies for minifb and rand.
    - Enhanced main.rs to include additional modules (dma, interrupts) and refactored the main loop for rendering.
    - Implemented a basic pixel buffer using random colors for window updates.
    - Cleaned up ppu.rs by simplifying the Ppu struct definition.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,48 @@
+use minifb::{Key, Scale, Window, WindowOptions};
 mod bus;
 mod cartridge;
 mod cpu;
+mod dma;
+mod interrupts;
 mod ppu;
 mod timer;
-mod interrupts;
+
 use bus::Bus;
+use cartridge::Cartridge;
 use cpu::Cpu;
-mod dma;
-use crate::cartridge::Cartridge;
+
+const WIDTH: usize = 160;
+const HEIGHT: usize = 144;
 
 fn main() {
+    let mut window = Window::new(
+        "Rust GB - Tetris",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            scale: Scale::X4,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.set_target_fps(10);
+
+    // format: 0x00RRGGBB (Red, Green, Blue)
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
     let bus = Bus::new(Cartridge::new("tetris.gb").unwrap());
     let mut cpu = Cpu::new(bus);
 
-    loop {
-        let cycles = cpu.step();
-        cpu.bus.tick(cycles);
-        if cycles == 0 {
-            println!("Crash detected!");
-            break;
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        cpu.step();
+
+        for pixel in buffer.iter_mut() {
+            let val = rand::random::<u8>() as u32;
+            *pixel = (val << 16) | (val << 8) | val;
         }
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
